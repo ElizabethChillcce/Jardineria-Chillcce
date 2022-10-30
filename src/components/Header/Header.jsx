160e481efe9c6cb68e329ca070030c44417f67b9
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-10-29 23:14:17 -0500

    Implementando imágenes, carrusel, algunos estilos, container, footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,20 +5,23 @@ import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
 
 export const Header = () => (
-  <><Navbar>
-    <Navbar.Brand>
-    <img src={logo} className="laptop" alt="logo" />
-    </Navbar.Brand>
+  <><Navbar style={{ backgroundColor: "yellow" }}>
+      <Navbar.Brand>
+        <img src={logo} className="laptop" alt="logo" style={{ width:"200px", height:"200px"}} />
+      </Navbar.Brand>
   
-  <Nav pullRight>
-      <Nav.Item icon={<HomeIcon />}><Link to="/" >Inicio</Link></Nav.Item>
-      <Nav.Item><Link to="/about" >Nosotros</Link></Nav.Item>
-      <Nav.Item>Servicios</Nav.Item>
-      <Nav.Item><Link to="/projects" >Galeria</Link></Nav.Item>
-      <Nav.Item>Blog</Nav.Item>
-      <Nav.Item>Contacto</Nav.Item>
-    </Nav>
-    </Navbar></>
+      <Nav pullRight>
+        <Nav.Item icon={<HomeIcon />}><Link to="/" >Inicio</Link></Nav.Item>
+        <Nav.Item><Link to="/about" >Nosotros</Link></Nav.Item>
+        <Nav.Menu title="Servicios">
+          <Nav.Item>Decoración con plantas</Nav.Item>
+          <Nav.Item>Diseño y mantenimiento de jardines</Nav.Item>
+        </Nav.Menu>
+        <Nav.Item><Link to="/projects" >Galeria</Link></Nav.Item>
+        <Nav.Item><Link to="/blog" >Blog</Link></Nav.Item>
+        <Nav.Item><Link to="/contact" >Contacto</Link></Nav.Item>
+      </Nav>
+      </Navbar></>
 );
 
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 /* import { Link } from "react-router-dom"; */
 import { Header } from "../Header/Header";
-/* import logo from "../../images/logo.png"; */
+import { Carousel, Container, Content, Footer, Sidebar } from 'rsuite';
+import lanceadas from "../../images/lanceadas.jpg";
+import chiflera from "../../images/chiflera.jpg";
+import orejaElefante from "../../images/orejaElefante.png";
 
 export const Home = () => {
   return (
    <>
    <Header/>
-   {/* <img src={logo} className="laptop" alt="logo" /> */}
-   {/* <h1>Jardineria Chillcce</h1> */}
-   </>
+   <Carousel autoplay className="custom-slider">
+    <img src={orejaElefante} height="250" alt="logo" />
+    <img src={lanceadas} height="250" alt="logo" />
+    <img src={chiflera} height="250" alt="logo" />
+    </Carousel>
+
+    <div className="show-container">
+
+    <Container>
+      {/* <Header>Header</Header> */}
+      <Container>
+        <Content>Bienvenidos</Content>
+        <Sidebar>Foto</Sidebar>
+      </Container>
+      <Footer>Pie de Página</Footer>
+    </Container>
+  </div>
+
+  </>
   );
 }
